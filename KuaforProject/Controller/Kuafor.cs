3b0d76625f2e49ae50373c3bdbe00492443eb095
Sara538112 <sara538112@gmail.com>
2024-12-10 22:32:40 +0300

    Update Kuafor.cs

--- KuaforProject/Controller/Kuafor.cs
+++ KuaforProject/Controller/Kuafor.cs
@@ -1,1 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace b221210566_2_.Controllers
+{
+    public class Kuafor : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+        public IActionResult Servies()
+        {
+            return View();
+        }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+    }
+}
