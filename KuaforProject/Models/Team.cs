f14204953e7dd840a93c07b538b0f51077231ed1
Sara538112 <sara538112@gmail.com>
2024-12-13 22:52:50 +0300

    Add files via upload

---
+++ KuaforProject/Models/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace b221210566_2_.Models
+{
+    public class Team
+    {
+        public ICollection<EmployeeExample> Employees { get; set; }
+        public ICollection<SupervisorExample> Supervisor { get; set; }
+        public ICollection<ManagerExample> Manager { get; set; }
+    }
+}
