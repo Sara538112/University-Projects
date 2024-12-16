929fe070388e51455de4ffff59c5dd25e6a8d02d
Sara538112 <sara538112@gmail.com>
2024-12-16 23:32:49 +0300

    Add files via upload

---
+++ KuaforProject/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace b221210566_2_.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ KuaforProject/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace b221210566_2_.Models
+{
+    public class User : IdentityUser
+    {
+        //public Department Department { get; set; }
+    }
+}
